a23b7779c448ddd08639b0d3bab6d5752bb8fa8f
Anish Panda <anishpanda2006@gmail.com>
2026-07-27 21:51:20 +0530

    small change

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="app">
-      <h1>Hello from React Vite!</h1>
+      <h1>Hello from React Vite developed by Anish!</h1>
       <p>This is a minimal Vite-powered React application.</p>
     </div>
   );
