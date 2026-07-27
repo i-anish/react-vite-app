fcfb147bbd9d0f32c2c80651a2a2a56afb5cacf4
Anish Panda <anishpanda2006@gmail.com>
2026-07-27 21:57:18 +0530

    another small change

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ function App() {
     <div className="app">
       <h1>Hello from React Vite developed by Anish!</h1>
       <p>This is a minimal Vite-powered React application.</p>
+      <p>This is a very complicated and sofiscated project ever made by the anish panda.....kudos to his efforts</p>
     </div>
   );
 }
