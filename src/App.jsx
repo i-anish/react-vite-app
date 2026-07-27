36c6f2f86c669f4721008730c3dc565f53e64055
Anish Panda <anishpanda2006@gmail.com>
2026-07-27 21:22:09 +0530

    built a minimal react vite app locally and commited

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello from React Vite!</h1>
+      <p>This is a minimal Vite-powered React application.</p>
+    </div>
+  );
+}
+
+export default App;
